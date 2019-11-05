b139804d742635a2019f7ee09f4f78f44bf7b718
Zohreh Ekhlasi <zekh167@UoA.auckland.ac.nz>
2019-11-05 16:40:57 +1300

    inital commit of the main

---
+++ src/workshop1/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def main():
+    test_array = np.array([[1, 2, 3, 4],
+                           [5, 6, 7, 8]])
+    return test_array
+
+
+if __name__ == '__main__':
+    ta = main()
+    print(ta)
